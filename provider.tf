08440f5a83288f24f1cef71c3e997fb667d3a382
Vitalii <this.azeroth@gmail.com>
2021-02-26 23:26:43 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   features {}
 
   #client_id       = "" #ARM_CLIENT_ID
-  #client_secret   = ""   #ARM_CLIENT_SECRET
+  #client_secret   = "" #ARM_CLIENT_SECRET
   #subscription_id = "" #ARM_SUBSCRIPTION_ID
   #tenant_id       = "" #ARM_TENANT_ID
 }
